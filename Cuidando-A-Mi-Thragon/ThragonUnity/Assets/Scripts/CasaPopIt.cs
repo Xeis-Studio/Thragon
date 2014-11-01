1383f61efd17e98848845cc5166cdfc1c0f8fbf1
Xeis-Studio <usrdes01@xeis-studio.com>
2014-11-01 07:22:07 -0600

    Proyecto

---
+++ Cuidando-A-Mi-Thragon/ThragonUnity/Assets/Scripts/CasaPopIt.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CasaPopIt : MonoBehaviour {
+
+	public Button comer;
+	public Button jugar;
+	public GuiControl controlGuiTiempo;
+
+	private GameObject thragon;
+	// Use this for initialization
+	void Start () {
+		thragon = GameObject.FindGameObjectWithTag("thragon");
+		//controlGuiTiempo = GameObject.FindObjectOfType<GuiControl>();
+	}
+
+	public void cerrarVentana(){
+		var agent = thragon.GetComponent<AgentScript>();
+		if(agent != null)
+		agent.canMove = true;
+		this.gameObject.SetActive(false);
+	}
+
+	public void irAComer()
+	{
+		GameControl.control.isEat = 1;
+		GameControl.control.isPlay = 0;
+		GameControl.control.Save();
+		//Application.LoadLevel(4);
+		AdministradorNiveles.cargarCocina();
+	}
+
+	public void irAJugar()
+	{
+		GameControl.control.isEat = 0;
+		GameControl.control.isPlay = 1;
+		GameControl.control.Save();
+		//Application.LoadLevel(5);
+		AdministradorNiveles.cargarEjercicio();
+	}
+
+	public void Salir()
+	{
+		GameControl.control.Save();
+		AdministradorNiveles.cargarInicio();
+	}
+
+	void OnEnable(){
+		if(controlGuiTiempo.canEat()){
+			comer.gameObject.SetActive(true);
+		}
+		if(controlGuiTiempo.canPlay()){
+			jugar.gameObject.SetActive(true);
+		}
+	}
+
+	void OnDisable(){
+		comer.gameObject.SetActive(false);
+		jugar.gameObject.SetActive(false);
+	}
+}
---
+++ Cuidando-A-Mi-Thragon/ThragonUnity/Assets/Scripts/GymRunning/SpinAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinAnimation : MonoBehaviour {
+
+	public float rotSpeed = 100;
+	public bool isRunning;
+
+	void Awake(){
+		isRunning = false;
+	}
+
+
+	IEnumerator animationCoorutine(){
+		if(!isRunning){
+			isRunning = true;
+			yield return new WaitForSeconds(8f);
+			isRunning = false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void StartAnimation(){
+		StartCoroutine(animationCoorutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isRunning){
+			transform.Rotate(rotSpeed*Time.deltaTime,0 , 0, Space.World);
+		}
+		else{
+			return;
+		}
+	}
+}
